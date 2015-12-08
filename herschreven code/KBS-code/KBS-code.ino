22756683d1a82e6dfa3eae2b2eeec8c0681e35c5
maestroke <formaestroke@gmail.com>
2015-12-08 15:15:30 +0100

    comments toegevoegt
    yay

--- herschreven code/KBS-code/KBS-code.ino
+++ herschreven code/KBS-code/KBS-code.ino
@@ -25,7 +25,7 @@ uint8_t zbutton, in_air, obstakelActief1, toJump, gameStart, buttonPressed, game
 
 
 void tekenLijn() {
-  lcd.fillRect(0, 160, 320, 32, RGB(0, 100, 0));
+  lcd.fillRect(0, 160, 320, 32, RGB(0, 100, 0));// tekent de grond waarop je rent
 }
 
 void obstakel(int x) {
@@ -42,7 +42,8 @@ void sidescroll() {
     obstakelActief1 = 1;
     obstakelLocatie1 = 320;
   }
-  if (obstakelActief1 == 1) {
+  //genereert een obstakel
+  if (obstakelActief1 == 1) {     //als een obstakel actief is, scrol de game
     
     //obstakel(obstakelLocatie1);
 
@@ -51,13 +52,14 @@ void sidescroll() {
     if (toJump == 0) {
       checkJump();
     }
+    //als er moet worden gesprongen, srpint
     //_delay_ms(3);
     if (obstakelLocatie1 == -32) {
       score++;
       obstakelLocatie1 = 320;
       obstakelActief1 = 0;
     }
-
+    //als de obstakel uit het scherm is, haal hem weg en geef de speler 1 punt
     obstakelLocatie1--;
   }
   //resetObstakel(last_x);
@@ -72,20 +74,20 @@ void checkJump(){
     }
   }
 }
-
+//kijkt of er moet worden gesprongen
 void speler() {
   lcd.fillRect(32, 140, 20, 20, RGB(0, 0, 255)); //vierkant
 }
-
+// tekent de speler
 
 void jump(){
   in_air = 1;
-  if(directie == 1){
+  if(directie == 1){      //kijkt of de speler omhoof moet
     lcd.fillRect(32, (current + 1), 20, 20, RGB(255,255,255)); //verwijder vorige ball
     lcd.fillRect(32, current, 20, 20, RGB(0,0,255));  //ball omhoog
     if(jumpLoopCount == 0){
       i = 0;
-    }
+    }//als het de eerste keer is dat je omhoog springt loopt, zet i op 0 zodat we kunnen bijhouden of we helemaal boven zijn
     _delay_ms(1);
     current--;  //speler tekent bolletje steeds opnieuw vind oplossing
     i++;
@@ -93,7 +95,7 @@ void jump(){
     if(i == up){
       directie = 0;
       jumpLoopCount = 0;
-    }
+    }//als de speler helemaal bovenaan is, zet de directie op 0 en zeg dat hij voor de eerste keer omlaag wordt getekent
   }else if(directie == 0){
     if(jumpLoopCount == 0){
       i = 0;
@@ -196,9 +198,9 @@ void inputScore(){
   tekenVak3();
   
   scoreSubmit = 1;
-  while(scoreSubmit){
+  while(scoreSubmit){ //loopt zolang je nog bezig bent om je score in te vullen
     checkButtonPress();
-    if(charverandering == 1){
+    if(charverandering == 1){ //kijkt of er een character is verandert, en zo ja, hertekent de characters
       gameStart = 0;
       
       tekenVak1();
@@ -235,52 +237,52 @@ void checkButtonPress(){
         }
       }
       if(postGame == 1){
-        if(lcd.touchX() > 70 && lcd.touchX() < 120 && lcd.touchY() > 70 && lcd.touchY() < 122){
+        if(lcd.touchX() > 70 && lcd.touchX() < 120 && lcd.touchY() > 70 && lcd.touchY() < 122){// kijkt of eerste character omhoog moet
           if(eerste == 'Z'){
             eerste = 'A';
           }else{
             eerste++;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 70 && lcd.touchX() < 120 && lcd.touchY() > 122 && lcd.touchY() < 175){
+        }else if(lcd.touchX() > 70 && lcd.touchX() < 120 && lcd.touchY() > 122 && lcd.touchY() < 175){// kijkt of eerste character omlaag moet
           if(eerste == 'A'){
             eerste = 'Z';
           }else{
             eerste--;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 130 && lcd.touchX() < 180 && lcd.touchY() > 70 && lcd.touchY() < 122){
+        }else if(lcd.touchX() > 130 && lcd.touchX() < 180 && lcd.touchY() > 70 && lcd.touchY() < 122){// kijkt of tweede character omhoog moet
           if(tweede == 'Z'){
             tweede = 'A';
           }else{
             tweede++;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 130 && lcd.touchX() < 180 && lcd.touchY() > 122 && lcd.touchY() < 175){
+        }else if(lcd.touchX() > 130 && lcd.touchX() < 180 && lcd.touchY() > 122 && lcd.touchY() < 175){// kijkt of tweede character omlaag moet
           if(tweede == 'A'){
             tweede = 'Z';
           }else{
             tweede--;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 190 && lcd.touchX() < 240 && lcd.touchY() > 70 && lcd.touchY() < 122){
+        }else if(lcd.touchX() > 190 && lcd.touchX() < 240 && lcd.touchY() > 70 && lcd.touchY() < 122){// kijkt of derde character omhoog moet
           if(derde == 'Z'){
             derde = 'A';
           }else{
             derde++;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 190 && lcd.touchX() < 240 && lcd.touchY() > 122 && lcd.touchY() < 175){
+        }else if(lcd.touchX() > 190 && lcd.touchX() < 240 && lcd.touchY() > 122 && lcd.touchY() < 175){//kijkt of derde character omlaag moet
           if(derde == 'A'){
             derde = 'Z';
           }else{
             derde--;
           }
           charverandering = 1;
-        }else if(lcd.touchX() > 22 && lcd.touchX() < 192 && lcd.touchY() > 164 && lcd.touchY() < 189){
+        }else if(lcd.touchX() > 22 && lcd.touchX() < 192 && lcd.touchY() > 164 && lcd.touchY() < 189){//kijkt of de score moet worden opgeslagen
           scoreSubmit = 0;
           charverandering = 1;
-        }else if(lcd.touchX() > 210 && lcd.touchX() < 290 && lcd.touchY() > 164 && lcd.touchY() < 189){
+        }else if(lcd.touchX() > 210 && lcd.touchX() < 290 && lcd.touchY() > 164 && lcd.touchY() < 189){//kijkt of de score niet moet worden opgeslagen
           scoreSubmit = 0;
           charverandering = 1;
         }
@@ -366,7 +368,7 @@ void game() {
   //alles resetten
 }
 void hitbox(){
-  if(spelerRechterZijde > obstakelLocatie1 && current+20 > obstakelBovenkant && 32 < (obstakelLocatie1 + 32)){
+  if(spelerRechterZijde > obstakelLocatie1 && current+20 > obstakelBovenkant && 32 < (obstakelLocatie1 + 32)){// kijkt of de speler het obstakel aanraakt
     death++;
   }
 }
