9f1900788c146ac23bff8b192f271c7f4eb5fe6f
maestroke <formaestroke@gmail.com>
2015-12-10 09:49:03 +0100

    hitbox aangepast
    je kan nu op de vierkanten landen

--- blokjesTest/blokjesTest.ino
+++ blokjesTest/blokjesTest.ino
@@ -9,207 +9,207 @@
 #include <util/delay.h>
 
 MI0283QT9 lcd;
-int up = 45;
-int zbutton = 0;
-int in_air = 0;
-int obstakelLocatie1 = 0;
-int obstakelActief1 = 0;
-int directie = 1;
-int current = 140;
-int jumpLoopCount = 0;
-int i = 0;
-int toJump = 0;
-int gameStart = 0;
-int buttonPressed = 0;
-int firstTime = 1;
-int gameIsLive = 0;
-int death = 0;
-int postGame = 0;
-int topscore = 0;
-int top5 = 1;
-int scoreSubmit = 1;
-char eerste = 'A';
-char tweede = 'B';
-char derde = 'C';
-String eerste2;
-String tweede2;
-String derde2;
-int charverandering = 0;
-int toCheckButton = 1;
-int scoresBack = 0;
-int obstakelBovenkant = 128;
-int spelerRechterZijde = 52;
+//getallen:
+uint16_t obstakelLocatie1, jumpLoopCount, score, i, last_x, x, topscore, obstakelBovenkant = 128, spelerRechterZijde = 53, current = 139;
+uint8_t up = 70;
 
-void pixel(int x, int y, String kleur){
-  lcd.drawRect(x,y,16,16,RGB(255,255,255));       //wit randje
-  
-  if(kleur == "groen"){               //onderste rij is groen
-    lcd.fillRect(x,y,15,15,RGB(0,100,0));
-    }
-    else{
-    lcd.fillRect(x,y,15,15,RGB(0,0,0));
-  }
-  
+//namen:
+char eerste = 'A', tweede = 'B', derde = 'C';
+String eerste2, tweede2, derde2;
+
+//booleans:
+uint8_t firstTime = 1, top5 = 1, directie = 1, scoreSubmit = 1, eersteKeerScore = 1, toCheckButton = 1;
+uint8_t zbutton, in_air, obstakelActief1, toJump, gameStart, buttonPressed, gameIsLive, death, postGame,charverandering, scoresBack, bovenBlok = 0, jumpPause = 0;
+
+void tekenLijn() {
+  lcd.fillRect(0, 160, 320, 32, RGB(0, 100, 0));
 }
 
-void vulScherm(){
-  for(int j = 0; j < 320; j += 16){       //pixels op alle plekken op de x-as zetten
-    for(int k = 160; k < 240; k += 16){     //pixels op onderste rij y-as
-      pixel(j,k, "groen");
-    }
-  }
+void obstakel(int x) {
+  lcd.fillRect(x, 160 - (32), 1, 32, RGB(255, 0, 0)); //eerste rij genereren
 }
 
-void tekenLijn(){
-  lcd.fillRect(0, 160,320,32,RGB(0,100,0));
+void resetObstakel(int x) {
+  lcd.fillRect(x + 32, 160 - (32), 1, 32, RGB(255, 255, 255)); //laatste rij van obstakel resetten
 }
 
-void obstakel(int x){
-  lcd.fillRect(x, 160 - (32),1,32, RGB(255,0,0));    //eerste rij genereren 
-}
-void resetObstakel(int x){
-  lcd.fillRect(x + 32, 160 - (32),1,32, RGB(255,255,255));    //laatste rij van obstakel resetten
-}
-
-
-
-void sidescroll(){
-  int last_x;
-  int x;
-  
-  if(obstakelActief1 == 0){
+void sidescroll() {
+  if (obstakelActief1 == 0) {
     obstakelActief1 = 1;
     obstakelLocatie1 = 320;
   }
-  if(obstakelActief1 == 1){
-    resetObstakel(last_x);
-    obstakel(obstakelLocatie1);
+  if (obstakelActief1 == 1) {
     
+    //obstakel(obstakelLocatie1);
+
     last_x = obstakelLocatie1;
-   
-    nunchuck_get_data();
-    zbutton = nunchuck_zbutton();
-    if(toJump == 0){
-      speler();
+
+    if (toJump == 0) {
+      checkJump();
     }
-    //_delay_ms(0);
-    if(obstakelLocatie1 == -32){
+    //_delay_ms(3);
+    if (obstakelLocatie1 == -32) {
+      score++;
+      lcd.fillRect(105, 210, 20, 20, RGB(255, 255, 255));
+      lcd.drawInteger(105, 210, score, DEC, RGB(0,0,0), RGB(255, 255, 255), 2);
       obstakelLocatie1 = 320;
       obstakelActief1 = 0;
+      bovenBlok = 0;
     }
-     
+
     obstakelLocatie1--;
   }
-  resetObstakel(last_x);
+  //resetObstakel(last_x);
 }
 
-void speler(){
-  lcd.fillRect(32, current, 20, 20, RGB(0,0,255)); //bolletje
-  while(zbutton == 1){
-  if (zbutton == 1 && in_air == 0) {
-    toJump = 1;
-    zbutton = 0;
+void checkJump(){
+  while (zbutton == 1) {
+    if (zbutton == 1 && in_air == 0) {
+      Serial.print("test speler");
+      toJump = 1;
+      zbutton = 0;
+    }
   }
-  }
+}
+
+void speler() {
+  lcd.fillRect(32, 140, 20, 20, RGB(0, 0, 255)); //vierkant
 }
 
 void jump(){
   in_air = 1;
-  if(directie == 1){
-    if(jumpLoopCount == 0){
-      i = 0;
-    }
-    lcd.fillRect(32, current + 1, 20, 20, RGB(255,255,255)); //verwijder vorige ball
-    lcd.fillRect(32, current, 20, 20, RGB(0,0,255));  //ball omhoog
-    _delay_ms(1);
-    current--;  //speler tekent bolletje steeds opnieuw vind oplossing
-    i++;
-    jumpLoopCount++;
-    if(i == up){
-      directie = 0;
-      jumpLoopCount = 0;
-    }
-  }else if(directie == 0){
-    if(jumpLoopCount == 0){
-      i = 0;
-    }
-    lcd.fillRect(32, (current - 1), 20, 20, RGB(255,255,255)); //verwijder vorige ball
-    lcd.fillRect(32, current, 20, 20, RGB(0,0,255));  //ball omlaag
-    _delay_ms(1);
-    current++;
-    i++;
-    jumpLoopCount++;
-    if(i == up){
-      directie = 1;
-      jumpLoopCount = 0;
-      toJump = 0;
+  if(!jumpPause){
+    if(directie == 1){
+      lcd.fillRect(32, (current + 1), 20, 20, RGB(255,255,255)); //verwijder vorige ball
+      lcd.fillRect(32, current, 20, 20, RGB(0,0,255));  //ball omhoog
+      if(jumpLoopCount == 0){
+        i = 0;
+      }
+      _delay_ms(1);
+      current--;  //speler tekent bolletje steeds opnieuw vind oplossing
+      i++;
+      jumpLoopCount++;
+      if(i == up){
+        directie = 0;
+        jumpLoopCount = 0;
+      }
+    }else if(directie == 0){
+      if(jumpLoopCount == 0){
+        i = 0;
+      }
+      lcd.fillRect(32, (current - 1),20,20 , RGB(255,255,255)); //verwijder vorige ball
+      lcd.fillRect(32, current, 20, 20, RGB(0,0,255));  //ball omlaag
+      _delay_ms(1);
+      current++;
+      i++;
+      jumpLoopCount++;
+      if(i == up){
+        directie = 1;
+        jumpLoopCount = 0;
+        toJump = 0;
+      }
     }
   }
   in_air = 0;
+}
 
-  }
-  
-  
-void drawMenu(){
+void drawScores(){
   lcd.fillScreen(RGB(111,111,111));
   lcd.fillRect(0, 160,320,32,RGB(0,50,0));
   //achtergrond van het scherm
   
-  lcd.drawText(10, 10, "THE NOT SO POSSIBLE", RGB(190,0,0), RGB(111,111,111), 2);
-  lcd.drawText(125, 30, "GAME!", RGB(190,0,0), RGB(111,111,111), 2);
-  //tekent de menu tekst met de game naam
-  
-  lcd.fillRoundRect(110, 60, 100, 25, 5, RGB(0,034,255));
-  lcd.drawRoundRect(110, 60, 100, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(109, 59, 102, 27, 5, RGB(0,0,0));
-  lcd.drawText(122, 65, "START", RGB(0,0,0), RGB(0,034,255), 2);
-  //tekent de start knop
-  
-  lcd.fillRoundRect(110, 90, 100, 25, 5, RGB(0,034,255));
-  lcd.drawRoundRect(110, 90, 100, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(109, 89, 102, 27, 5, RGB(0,0,0));
-  lcd.drawText(113, 95, "SCORES", RGB(0,0,0), RGB(0,034,255), 2);
-  //tekent de scores knop
-  
-  lcd.fillRoundRect(110, 120, 100, 25, 5, RGB(0,034,255));
-  lcd.fillRoundRect(110, 145, 100, 25, 5, RGB(0,255,255));
-  lcd.fillRect(110, 125, 100, 20, RGB(0,034,255));
-  lcd.fillRect(110, 145, 100, 20, RGB(0,255,255));
-  lcd.drawRoundRect(110, 120, 100, 50, 5, RGB(0,0,0));
-  lcd.drawRoundRect(109, 119, 102, 52, 5, RGB(0,0,0));
-  lcd.drawText(122, 125, "MULTI", RGB(0,0,0), RGB(0,034,255), 2);
-  lcd.drawText(112, 150, "PLAYER", RGB(0,0,0), RGB(0,255,255), 2);
-  //tekent de multiplayer knop
+  lcd.fillRoundRect(10, 200, 100, 25, 5, RGB(0,034,255));
+  lcd.drawRoundRect(10, 200, 100, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(9, 199, 102, 27, 5, RGB(0,0,0));
+  lcd.drawText(28, 205, "BACK", RGB(0,0,0), RGB(0,034,255), 2);
+  //back knop
+  while(scoresBack == 0){
+    checkButtonPress();
+  }
+  scoresBack = 0;
 }
 
-void hitbox(){
-  if(spelerRechterZijde > obstakelLocatie1 && current+20 > obstakelBovenkant){
-    death++;
-  }
+void tekenVak1(){
+  eerste2 = (String)eerste;
+  lcd.fillRoundRect(90, 115, 30, 25, 5, RGB(255,255,255));
+  lcd.drawRoundRect(90, 115, 30, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(89, 114, 32, 27, 5, RGB(0,0,0));
+  lcd.drawText(98, 120, eerste2, RGB(0,0,0), RGB(255,255,255), 2);
+  lcd.fillTriangle(97, 110, 112, 110, 104, 102, RGB(0,0,0));
+  lcd.fillTriangle(97, 145, 112, 145, 104, 153, RGB(0,0,0));
+  //tekent het eerste vak om je initialen in te vullen
 }
 
-void game(){
-  lcd.fillScreen(RGB(255,255,255)); // scherm leeg
-  tekenLijn();
-  nunchuck_setpowerpins();
-  nunchuck_init();
+void tekenVak2(){
+  tweede2 = (String)tweede;
+  lcd.fillRoundRect(140, 115, 30, 25, 5, RGB(255,255,255));
+  lcd.drawRoundRect(140, 115, 30, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(139, 114, 32, 27, 5, RGB(0,0,0));
+  lcd.drawText(148, 120, tweede2, RGB(0,0,0), RGB(255,255,255), 2);
+  lcd.fillTriangle(147, 110, 162, 110, 154, 102, RGB(0,0,0));
+  lcd.fillTriangle(147, 145, 162, 145, 154, 153, RGB(0,0,0));
+  //tekent het tweede vak om je initialen in te vullen
+}
 
-  while(gameIsLive == 1){
-    zbutton = nunchuck_zbutton();
-    nunchuck_get_data();
-    sidescroll();
-    if(toJump == 1){
-      jump();
-    }
-    hitbox();
-    if(death == 1){
-      gameIsLive = 0;
-    }
+void tekenVak3(){
+  derde2 = (String)derde;
+  lcd.fillRoundRect(190, 115, 30, 25, 5, RGB(255,255,255));
+  lcd.drawRoundRect(190, 115, 30, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(189, 114, 32, 27, 5, RGB(0,0,0));
+  lcd.drawText(198, 120, derde2, RGB(0,0,0), RGB(255,255,255), 2);
+  lcd.fillTriangle(197, 110, 212, 110, 204, 102, RGB(0,0,0));
+  lcd.fillTriangle(197, 145, 212, 145, 204, 153, RGB(0,0,0));
+  //tekent het derde vak om je initialen in te vullen
+}
+
+void inputScore(){
+  lcd.fillScreen(RGB(111,111,111));
+  lcd.fillRect(0, 160,320,32,RGB(0,50,0));
+  //achtergrond van het scherm
+  
+  lcd.drawText(82, 20, "GAME OVER", RGB(0,0,0), RGB(111,111,111), 2);
+  lcd.drawText(80, 50, "SCORE:", RGB(0,0,0), RGB(111,111,111), 2);
+  lcd.drawInteger(200, 50, score, DEC, RGB(0,0,0), RGB(111,111,111), 2);
+  //schrijft de tekst op het game over scherm als je dood gaat
+  
+  if(topscore == 1){
+    lcd.drawText(80, 80, "HIGHSCORE!", RGB(0,0,0), RGB(111,111,111), 2);
+  }else if(top5 == 1){
+    lcd.drawText(110, 80, "TOP 5!", RGB(0,0,0), RGB(111,111,111), 2);
   }
-  current = 140;
-  obstakelLocatie1 = 0;
-  obstakelActief1 = 0;
-  //alles resetten
+  //meldt of je de highscore hebt of dat je in de top 5 bent gekomen
+  
+  lcd.fillRoundRect(22, 164, 170, 25, 5, RGB(0,034,255));
+  lcd.drawRoundRect(22, 164, 170, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(21, 163, 172, 27, 5, RGB(0,0,0));
+  lcd.drawText(27, 170, "SAVE SCORE", RGB(0,0,0), RGB(0,034,255), 2);
+  //tekent de save score knop
+  
+  lcd.fillRoundRect(210, 164, 80, 25, 5, RGB(0,034,255));
+  lcd.drawRoundRect(210, 164, 80, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(209, 163, 82, 27, 5, RGB(0,0,0));
+  lcd.drawText(220, 170, "QUIT", RGB(0,0,0), RGB(0,034,255), 2);
+  //tekent de quit knop
+  
+  tekenVak1();
+  tekenVak2();
+  tekenVak3();
+  
+  scoreSubmit = 1;
+  while(scoreSubmit){
+    checkButtonPress();
+    if(charverandering == 1){
+      gameStart = 0;
+      
+      tekenVak1();
+      tekenVak2();
+      tekenVak3();
+      
+      charverandering = 0;
+    }
+    _delay_ms(100);
+  }
+  Serial.println("check1");
 }
 
 void checkButtonPress(){
@@ -292,114 +292,128 @@ void checkButtonPress(){
   }
 }
 
-void drawScores(){
-  lcd.fillScreen(RGB(111,111,111));
-  lcd.fillRect(0, 160,320,32,RGB(0,50,0));
-  //achtergrond van het scherm
-
-  lcd.fillRoundRect(10, 200, 100, 25, 5, RGB(0,034,255));
-  lcd.drawRoundRect(10, 200, 100, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(9, 199, 102, 27, 5, RGB(0,0,0));
-  lcd.drawText(28, 205, "BACK", RGB(0,0,0), RGB(0,034,255), 2);
-  //back knop
-  
-  while(scoresBack == 0){
-    checkButtonPress();
-  }
-  scoresBack = 0;
-}
-
-void tekenVak1(){
-  eerste2 = (String)eerste;
-  lcd.fillRoundRect(90, 115, 30, 25, 5, RGB(255,255,255));
-  lcd.drawRoundRect(90, 115, 30, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(89, 114, 32, 27, 5, RGB(0,0,0));
-  lcd.drawText(98, 120, eerste2, RGB(0,0,0), RGB(255,255,255), 2);
-  lcd.fillTriangle(97, 110, 112, 110, 104, 102, RGB(0,0,0));
-  lcd.fillTriangle(97, 145, 112, 145, 104, 153, RGB(0,0,0));
-  //tekent het eerste vak om je initialen in te vullen
-}
-
-void tekenVak2(){
-  tweede2 = (String)tweede;
-  lcd.fillRoundRect(140, 115, 30, 25, 5, RGB(255,255,255));
-  lcd.drawRoundRect(140, 115, 30, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(139, 114, 32, 27, 5, RGB(0,0,0));
-  lcd.drawText(148, 120, tweede2, RGB(0,0,0), RGB(255,255,255), 2);
-  lcd.fillTriangle(147, 110, 162, 110, 154, 102, RGB(0,0,0));
-  lcd.fillTriangle(147, 145, 162, 145, 154, 153, RGB(0,0,0));
-  //tekent het tweede vak om je initialen in te vullen
-}
-
-void tekenVak3(){
-  derde2 = (String)derde;
-  lcd.fillRoundRect(190, 115, 30, 25, 5, RGB(255,255,255));
-  lcd.drawRoundRect(190, 115, 30, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(189, 114, 32, 27, 5, RGB(0,0,0));
-  lcd.drawText(198, 120, derde2, RGB(0,0,0), RGB(255,255,255), 2);
-  lcd.fillTriangle(197, 110, 212, 110, 204, 102, RGB(0,0,0));
-  lcd.fillTriangle(197, 145, 212, 145, 204, 153, RGB(0,0,0));
-  //tekent het derde vak om je initialen in te vullen
-}
-
-void inputScore(){
+void drawMenu(){
   lcd.fillScreen(RGB(111,111,111));
   lcd.fillRect(0, 160,320,32,RGB(0,50,0));
   //achtergrond van het scherm
   
-  lcd.drawText(82, 20, "GAME OVER", RGB(0,0,0), RGB(111,111,111), 2);
-  lcd.drawText(80, 50, "SCORE:", RGB(0,0,0), RGB(111,111,111), 2);
-  lcd.drawInteger(200, 50, 10, DEC, RGB(0,0,0), RGB(111,111,111), 2);
-  //schrijft de tekst op het game over scherm als je dood gaat
+  lcd.drawText(10, 10, "THE NOT SO POSSIBLE", RGB(190,0,0), RGB(111,111,111), 2);
+  lcd.drawText(125, 30, "GAME!", RGB(190,0,0), RGB(111,111,111), 2);
+  //tekent de menu tekst met de game naam
   
-  if(topscore == 1){
-    lcd.drawText(80, 80, "HIGHSCORE!", RGB(0,0,0), RGB(111,111,111), 2);
-  }else if(top5 == 1){
-    lcd.drawText(110, 80, "TOP 5!", RGB(0,0,0), RGB(111,111,111), 2);
+  lcd.fillRoundRect(110, 60, 100, 25, 5, RGB(0,034,255));
+  lcd.drawRoundRect(110, 60, 100, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(109, 59, 102, 27, 5, RGB(0,0,0));
+  lcd.drawText(122, 65, "START", RGB(0,0,0), RGB(0,034,255), 2);
+  //tekent de start knop
+  
+  lcd.fillRoundRect(110, 90, 100, 25, 5, RGB(0,034,255));
+  lcd.drawRoundRect(110, 90, 100, 25, 5, RGB(0,0,0));
+  lcd.drawRoundRect(109, 89, 102, 27, 5, RGB(0,0,0));
+  lcd.drawText(113, 95, "SCORES", RGB(0,0,0), RGB(0,034,255), 2);
+  //tekent de scores knop
+  
+  lcd.fillRoundRect(110, 120, 100, 25, 5, RGB(0,034,255));
+  lcd.fillRoundRect(110, 145, 100, 25, 5, RGB(0,255,255));
+  lcd.fillRect(110, 125, 100, 20, RGB(0,034,255));
+  lcd.fillRect(110, 145, 100, 20, RGB(0,255,255));
+  lcd.drawRoundRect(110, 120, 100, 50, 5, RGB(0,0,0));
+  lcd.drawRoundRect(109, 119, 102, 52, 5, RGB(0,0,0));
+  lcd.drawText(122, 125, "MULTI", RGB(0,0,0), RGB(0,034,255), 2);
+  lcd.drawText(112, 150, "PLAYER", RGB(0,0,0), RGB(0,255,255), 2);
+  //tekent de multiplayer knop
+}
+
+void teken(){
+  if(obstakelLocatie1 != last_x){
+    obstakel(obstakelLocatie1);
+    resetObstakel(last_x);
   }
-  //meldt of je de highscore hebt of dat je in de top 5 bent gekomen
+}
+
+void game() {
+  lcd.fillScreen(RGB(255,255,255)); // scherm leeg
+  lcd.drawText(10, 210, "Score:", RGB(0,0,0), RGB(255, 255, 255), 2);
+  lcd.drawInteger(105, 210, score, DEC, RGB(0,0,0), RGB(255, 255, 255), 2);
+  tekenLijn();
+  nunchuck_setpowerpins();
+  nunchuck_init();
+  speler();
   
-  lcd.fillRoundRect(22, 164, 170, 25, 5, RGB(0,034,255));
-  lcd.drawRoundRect(22, 164, 170, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(21, 163, 172, 27, 5, RGB(0,0,0));
-  lcd.drawText(27, 170, "SAVE SCORE", RGB(0,0,0), RGB(0,034,255), 2);
-  //tekent de save score knop
-  
-  lcd.fillRoundRect(210, 164, 80, 25, 5, RGB(0,034,255));
-  lcd.drawRoundRect(210, 164, 80, 25, 5, RGB(0,0,0));
-  lcd.drawRoundRect(209, 163, 82, 27, 5, RGB(0,0,0));
-  lcd.drawText(220, 170, "QUIT", RGB(0,0,0), RGB(0,034,255), 2);
-  //tekent de quit knop
-  
-  tekenVak1();
-  tekenVak2();
-  tekenVak3();
-  
-  scoreSubmit = 1;
-  while(scoreSubmit){
-    checkButtonPress();
-    if(charverandering == 1){
-      gameStart = 0;
-      
-      tekenVak1();
-      tekenVak2();
-      tekenVak3();
-      
-      charverandering = 0;
+  while(gameIsLive == 1){
+    nunchuck_get_data();
+    zbutton = nunchuck_zbutton();
+    
+    sidescroll();
+    teken();
+    if(toJump == 1){
+      jump();
     }
-    _delay_ms(100);
+    hitbox();
+    if(death == 1){
+      gameIsLive = 0;
+    }
+    
   }
-  Serial.println("check1");
+  current = 139;
+  obstakelLocatie1 = 0;
+  obstakelActief1 = 0;
+  toJump = 0;
+  in_air = 0;
+  directie = 1;
+  jumpLoopCount = 0;
+  //alles resetten
 }
 
-int main(){
+void hitbox(){
+  if(spelerRechterZijde > obstakelLocatie1){
+    if(current+19 == obstakelBovenkant){
+      jumpPause++;
+    }
+    if(current+19 > obstakelBovenkant){
+      death++;
+    }
+  }
+  if(32 < obstakelLocatie1){
+    jumpPause = 0;
+  }
+
+//  if(spelerRechterZijde > obstakelLocatie1){
+//    if(current+20 > obstakelBovenkant && !bovenBlok){
+//      death++;
+//    }else if(current+19 == obstakelBovenkant){
+//      jumpPause++;
+//    }
+//  }
+//  if(current+20 == obstakelBovenkant){
+//    bovenBlok++;
+//  }
+//  if(current+20 > obstakelBovenkant && spelerRechterZijde > obstakelLocatie1){
+//    bovenBlok = 0;
+//  }
+//  if(bovenBlok && 12 > obstakelLocatie1){
+//    jumpPause++;
+//  }
+//  if(32 < (obstakelLocatie1)){
+//    jumpPause = 0;
+//  }
+//  && current+20 > obstakelBovenkant && 32 < (obstakelLocatie1 + 32)){
+//    if(!(spelerRechterZijde < obstakelLocatie1) && current+20 > obstakelBovenkant){
+//      
+//    }else{
+//      death++;
+//    }
+//  }
+}
+
+int main() {
   init();
-  Serial.begin(9600);
   lcd.begin();
   lcd.touchRead();
   lcd.touchStartCal(); //calibrate touchpanel
+
   
-  while(1){
+ while(1){
     if(firstTime == 1){
       drawMenu();                //drawed het menu
       firstTime = 0;
@@ -408,6 +422,7 @@ int main(){
     if(buttonPressed == 1){
       gameIsLive = 1;
       death--;
+      score = 0;
       game();
       firstTime = 1;
       buttonPressed = 10;
@@ -449,10 +464,13 @@ int main(){
       toCheckButton = 0;
       //tijdelijk, input van je naam scherm
     }
+    
     if(toCheckButton == 1){
       checkButtonPress();        //checked of er wordt gedrukt op een knop en kijkt waar dat is gebeurt
     }
+    
     toCheckButton = 1;
   }
+
   return 0;
 }
