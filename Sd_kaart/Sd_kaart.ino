d9bc7a76d5ed2c4a5285925649e5ce15cc4a556f
rickwinters1 <rickwinters1@hotmail.com>
2015-12-16 11:07:38 +0100

    kan lezen van EEPROM

--- Sd_kaart/Sd_kaart.ino
+++ Sd_kaart/Sd_kaart.ino
@@ -1,21 +1,24 @@
 #include <EEPROM.h>
 
+float f;
+
 struct Score{
   uint16_t score;
   char naam[10];
-  };
+};
 
-
+Score nummer1 = {30,"Piet"};
+Score nummer2 = {20, "DONO"};
 void saveScore(){
-  Score nummer1 = {30, "Piet"};
+
+  //Serial.println("voor de eeprom");
   EEPROM.put(0, nummer1);
-  Serial.println("STruct is succesvol geschreven");
+  //Serial.println("STruct is succesvol geschreven");
 }
 void printScore(){
-  Score nummer2 = {20, "DONO"};
+  
   EEPROM.get(0, nummer2);
-  Serial.println(nummer2.score);
-  Serial.println(nummer2.naam);
+
 }
 
 int main(){
@@ -23,5 +26,7 @@ int main(){
   Serial.begin(9600);
   saveScore();
   printScore();
+  Serial.println(nummer2.score);
+  Serial.println(nummer2.naam);
   return 0;
 }
