826feeea8a8f9016636051d47cdc82874c274637
Dono <unconfigured@null.spigotmc.org>
2016-01-11 15:28:53 +0100

    Revert "valt door bovenkant heen"
    This reverts commit 15d180a797c37cb54f279f608faeee17b3f47700.
    
    # Conflicts:
    #	code V3.1/herschreven_code_V3.1/Game.cpp

--- code V3.1/herschreven_code_V3.1/Game.cpp
+++ code V3.1/herschreven_code_V3.1/Game.cpp
@@ -12,13 +12,13 @@
 #include "Menu.h"
 #include "Jump.h"
 #include "Opmaak.h"
+  
 
-
-
+  
 void Game::game(MI0283QT9 lcd) {
-  Jump J;
-  Menu M;
-  Opmaak O;
+Jump J;
+Menu M;
+Opmaak O;
 
   lcd.fillScreen(RGB(255, 255, 255)); // scherm leeg
   lcd.drawText(10, 210, "Score:", RGB(0, 0, 0), RGB(255, 255, 255), 2);
@@ -33,6 +33,7 @@ void Game::game(MI0283QT9 lcd) {
   O.obstakelLocatie1 = 0;
   O.obstakelLocatie2 = 0;
   O.aantalObstakels = 0;
+<<<<<<< HEAD
 
 
   while (gameIsLive) {
@@ -48,16 +49,50 @@ void Game::game(MI0283QT9 lcd) {
    
     hitbox(O, J);
     J.tekenJump(lcd);
+=======
+  //J.in_air = false;
+  
+  while (gameIsLive) {
+    nunchuck_get_data();
+    J.zbutton = nunchuck_zbutton();
+    
+    //Serial.print(J.zbutton);
+    
+    O.randomLevel();
+    vormObstakel1 = O.obstakelVorm1;
+    locatieObstakel1 = O.obstakelLocatie1;
+    yPosition = J.positionY;
+    Serial.print(O.obstakelLocatie1);
+    Serial.print(" - ");
+    Serial.println(locatieObstakel1);
+    Serial.print(O.obstakelVorm1);
+    Serial.print(" - ");
+    Serial.print(vormObstakel1);
+    hitbox();
+>>>>>>> parent of 15d180a... valt door bovenkant heen
     O.sidescroll(lcd);
-
+    
     O.teken(lcd);
     J.checkJump();
+<<<<<<< HEAD
 
 
+=======
+    J.tekenJump(lcd);
+    
+//    Serial.println(death);
+//    Serial.println(J.positionY);
+//    Serial.println(O.obstakelLocatie1);
+//    Serial.print('\n');
+//    _delay_ms(100);
+    
+    
+>>>>>>> parent of 15d180a... valt door bovenkant heen
     if (death) {
       gameIsLive = false;
     }
   }
+<<<<<<< HEAD
 }
 void Game::hitbox(Opmaak P, Jump J) {
   if (P.obstakelVorm1 == 2) {
@@ -68,6 +103,42 @@ void Game::hitbox(Opmaak P, Jump J) {
    
   if (P.obstakelVorm1 == 1) {
     if (47 > P.obstakelLocatie1) {
+=======
+
+  
+}
+void Game::hitbox() {
+  Jump J;
+  Opmaak O;
+
+  Serial.print(" - ");
+  Serial.print(O.obstakelVorm1);
+  Serial.print(" - ");
+  Serial.println(vormObstakel1);
+  if (vormObstakel1 == 2) { //als eerste figuur == 4kant
+    Serial.println("vorm1 == 2");
+    if (47 > locatieObstakel1) {
+      Serial.println("47 > O.Obstakel1");
+      if (47 > locatieObstakel1 && yPosition > 128) {
+        death = true;
+      }
+      if (yPosition <= 128) {
+        J.velocityY = 0.0;
+        J.in_air = false;
+        J.positionY = 128;
+        geland = true;
+      }
+    }
+    if (32 < locatieObstakel1 && geland) {
+     J.in_air = true;
+    }
+    if(yPosition > 160){
+      J.in_air = false;
+    }
+  }
+  if (O.obstakelVorm1 == 1) {
+    if (47 > O.obstakelLocatie1) {
+>>>>>>> parent of 15d180a... valt door bovenkant heen
       if (J.positionY > currentY) {
         death = true;
       }
